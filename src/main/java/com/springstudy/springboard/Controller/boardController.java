721979dd4c78b1067535082d8721582f975c1804
최서연 <gkdltjdus@gmail.com>
2021-07-11 01:03:56 +0900

    The First Commit

--- src/main/java/com/springstudy/springboard/Controller/boardController.java
+++ src/main/java/com/springstudy/springboard/Controller/boardController.java
@@ -8,7 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class boardController {
     
     @GetMapping("Board")
-    public String Board(Model model) {
-        model.addAttribute("data", "board start")
+    public String Board(Model model) {  
+        String s_model = model.toString();
+        return s_model;
+
+    }
+
+    public String getTitle(String title) {
+        return title;
     }
 }
